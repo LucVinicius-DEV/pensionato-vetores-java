c973fc104c0fbc140d1aff8dfee8aad2406f864b
Lucas <us3.lucas@gmail.com>
2025-05-03 14:24:16 -0300

    Update Pensionato.java

--- Pensionato.java
+++ Pensionato.java
@@ -1,28 +1,5 @@
 import java.util.Scanner;
 
-class Estudante {
-    private String nome;
-    private String email;
-
-    public Estudante(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public String toString() {
-        return nome + ", " + email;
-    }
-}
-
 public class Pensionato {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
